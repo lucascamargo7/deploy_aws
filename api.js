9d24a9b781ac1e0d5af40a013d43f2128d88512d
Lucas Camargo <lucas.camargo7@fatec.sp.gov.br>
2020-11-09 15:09:07 -0300

    alterando caminho porta

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ app.use(require("cors")());
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-    res.json({message: "Tudo ok por aqui!"});
+    res.json({message: "Tudo ok por aqui CERTO!"});
 })
 
 app.post('/send', upload.single('anexo'), (req, res, next) => { 
@@ -23,5 +23,10 @@ app.post('/send', upload.single('anexo'), (req, res, next) => {
 }) 
 
 const server = http.createServer(app); 
-server.listen(3030);
-console.log("Servidor escutando na porta 3030...")
+//server.listen(3030);
+//console.log("Servidor escutando na porta 3030...")
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${ PORT }`);
+});
